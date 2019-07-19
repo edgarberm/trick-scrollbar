36ccc9888d47d3b0a68e973566e332999fefcfb8
builtbyedgar <hello@builtbyedgar.com>
2019-07-19 17:46:36 +0200

    wip

--- __tests__/trick-scrollbar.test.js
+++ __tests__/trick-scrollbar.test.js
@@ -2,31 +2,34 @@ import TrickScrollbar from '../src/trick-scrollbar'
 
 jest.mock('../src/trick-scrollbar')
 
-beforeEach(() => {
-  TrickScrollbar.mockClear()
-})
+describe('TrickScrollbar', () => {
+  beforeEach(() => {
+    TrickScrollbar.mockClear()
+  })
 
-function setDOM () {
-  document.body.innerHTML = '<div class="wrapper"><div class="scroll"></div></div>'
-}
-
-
-it('The TrickScrollbar instance can be created', () => {
-  const trickScrollbar = new TrickScrollbar()
-  expect(trickScrollbar).toBeTruthy()
-})
-
-it('Check if the class constructor is called', () => {
-  setDOM()
-  const scroller = document.querySelector('.scroll')
-  const trickScrollbar = new TrickScrollbar(scroller)
-  expect(TrickScrollbar).toHaveBeenCalledTimes(1)
-  expect(TrickScrollbar).toHaveBeenCalledWith(scroller);
-})
-
-it('Checks to see if the item is set correctly', () => {
-  setDOM()
-  const scroller = document.querySelector('.scroll')
-  const trickScrollbar = new TrickScrollbar(scroller)
-  expect(TrickScrollbar).toHaveBeenCalledWith(scroller);
+  afterEach(() => {
+    jest.resetModules()
+  })
+  
+  it('Instance can be created', () => {
+    const trickScrollbar = new TrickScrollbar()
+    expect(trickScrollbar).toBeTruthy()
+  })
+  
+  it('Check if the class constructor is called', () => {
+    const scroller = document.querySelector('.scroll')
+    const trickScrollbar = new TrickScrollbar(scroller)
+    expect(TrickScrollbar).toHaveBeenCalledTimes(1)
+    expect(TrickScrollbar).toHaveBeenCalledWith(scroller)
+  })
+  
+  it('Checks if the item is set correctly', () => {
+    const scroller = document.querySelector('.scroll')
+    const trickScrollbar = new TrickScrollbar(scroller)
+    expect(TrickScrollbar).toHaveBeenCalledWith(scroller) 
+  })
+  
+  it('Checks if the scrollbars is append correctly', () => {
+    // TODO
+  })
 })

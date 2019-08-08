d134efe2a5f2d4fd5ec4f7b96019435366aecc9c
builtbyedgar <hello@builtbyedgar.com>
2019-08-08 03:13:17 +0200

    some DOM tests #10

--- __tests__/trick-scrollbar.test.js
+++ __tests__/trick-scrollbar.test.js
@@ -1,12 +1,15 @@
 import TrickScrollbar from '../src/trick-scrollbar'
+import fs from 'fs'
 
+jest.dontMock('fs')
 jest.mock('../src/trick-scrollbar')
+const html = fs.readFileSync('example/index.html', 'utf8').toString()
 
 describe('TrickScrollbar', () => {
   beforeEach(() => {
     TrickScrollbar.mockClear()
   })
-
+  
   afterEach(() => {
     jest.resetModules()
   })
@@ -17,19 +20,57 @@ describe('TrickScrollbar', () => {
   })
   
   it('Check if the class constructor is called', () => {
+    document.documentElement.innerHTML = html
     const scroller = document.querySelector('.scroll')
-    const trickScrollbar = new TrickScrollbar(scroller) 
+    new TrickScrollbar(scroller)
+
     expect(TrickScrollbar).toHaveBeenCalledTimes(1)
     expect(TrickScrollbar).toHaveBeenCalledWith(scroller)
   })
   
-  it('Checks if the item is set correctly', () => {
+  it('Checks if inital methods are called', () => {
+    document.documentElement.innerHTML = html
     const scroller = document.querySelector('.scroll')
-    const trickScrollbar = new TrickScrollbar(scroller)
-    expect(TrickScrollbar).toHaveBeenCalledWith(scroller) 
+    new TrickScrollbar(scroller)
+    const mockTrickScrollbar = TrickScrollbar.mock.instances[0]
+    const mockAssembleDOM = mockTrickScrollbar.assembleDOM
+    const mockChechChildrenWidth = mockTrickScrollbar.chechChildrenWidth
+    const mockAppendThumbX = mockTrickScrollbar.appendThumbX
+    const mockAppendThumbY = mockTrickScrollbar.appendThumbY
+    const mockResizeScrollbarX = mockTrickScrollbar.resizeScrollbarX
+    const mockResizeScrollbarY = mockTrickScrollbar.resizeScrollbarY
+    const mockAddEventListeners = mockTrickScrollbar.addEventListeners
+    
+    setTimeout(() => {
+      expect(mockAssembleDOM).toHaveBeenCalledTimes(1)
+      expect(mockChechChildrenWidth).toHaveBeenCalledTimes(1)
+      expect(mockAppendThumbX).toHaveBeenCalledTimes(1)
+      expect(mockAppendThumbY).toHaveBeenCalledTimes(1)
+      expect(mockResizeScrollbarX).toHaveBeenCalledTimes(1)
+      expect(mockResizeScrollbarY).toHaveBeenCalledTimes(1)
+      expect(mockAddEventListeners).toHaveBeenCalledTimes(1)
+    }, 200)
   })
-  
-  it('Checks if the scrollbars is append correctly', () => {
-    // TODO
+
+  it('Checks if DOM is assembled correctly', () => {
+    document.documentElement.innerHTML = html
+    const scroller = document.querySelector('.scroll')
+    new TrickScrollbar(scroller)
+
+    setTimeout(() => {
+      const scrollContent = document.querySelector('.ts-scroll-content')
+      const scroller = scrollContent.querySelector('.ts-scroller')
+      const scrollbarX = scrollContent.querySelector('.ts-scrollbar-x')
+      const scrollbarY = scrollContent.querySelector('.ts-scrollbar-y')
+      const thumbX = scrollbarX.querySelector('.ts-thumb-x')
+      const thumbY = thumbY.querySelector('.ts-thumb-y')
+
+      expect(scrollContent).toBeEqual(HTMLDivElement)
+      expect(scroller).toBeEqual(HTMLDivElement)
+      expect(scrollbarX).toBeEqual(HTMLDivElement)
+      expect(scrollbarY).toBeEqual(HTMLDivElement)
+      expect(thumbX).toBeEqual(HTMLDivElement)
+      expect(thumbY).toBeEqual(HTMLDivElement)
+    }, 200)
   })
 })
